9113e2ae4d63577478b2d3ae85f1ad244ce86126
Sarvesh Sandeep Patil <sarveshpatilofficial@gmail.com>
2023-03-19 12:44:02 +0530

    oops

--- OOPS/src/com/sarvesh/abstraction/Main.java
+++ OOPS/src/com/sarvesh/abstraction/Main.java
@@ -1,4 +1,10 @@
 package com.sarvesh.abstraction;
 
 public class Main {
+    public static void main(String[] args) {
+        Son Krish = new Son();
+        Daughter Priya = new Daughter();
+        Krish.career("Engineer");
+        Priya.career("Doctor");
+    }
 }
